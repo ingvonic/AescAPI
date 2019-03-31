18a9cb483d11e6e9d88a2628dd3d69479db27148
DaveYognaught <daveoverzero@gmail.com>
2019-03-30 20:41:39 -0400

    Rendering and spritesheets

--- aesc/src/main/java/com/ingvonic/aesc/graphics/Sprite.java
+++ aesc/src/main/java/com/ingvonic/aesc/graphics/Sprite.java
@@ -1,4 +1,29 @@
 package com.ingvonic.aesc.graphics;
 
 public class Sprite {
+
+    private byte[][] raster;
+
+    private int w;
+    private int h;
+
+    public Sprite(SpriteSheet sheet, int x, int y, int w, int h) {
+        this.w = w;
+        this.h = h;
+        for (int xx = 0; xx < w; xx++)
+            for (int yy = 0; yy < h; yy++)
+                raster[xx][yy] = sheet.getRaster()[x * w + xx][y * h + yy];
+    }
+
+    public int getPixel(int x, int y) {
+        return raster[x][y] + 128;
+    }
+
+    public int getWidth() {
+        return w;
+    }
+
+    public int getHeight() {
+        return h;
+    }
 }
