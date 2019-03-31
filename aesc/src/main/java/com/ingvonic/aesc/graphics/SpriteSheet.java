b01858825a9ace43a4b779990e695583347b38bd
DaveYognaught <daveoverzero@gmail.com>
2019-03-30 22:18:34 -0400

    Removed <identifier> field from SpriteSheet.java

--- aesc/src/main/java/com/ingvonic/aesc/graphics/SpriteSheet.java
+++ aesc/src/main/java/com/ingvonic/aesc/graphics/SpriteSheet.java
@@ -2,22 +2,15 @@ package com.ingvonic.aesc.graphics;
 
 public class SpriteSheet {
 
-    private String identifier;
-
     private byte[][]raster;
 
     private int width;
     private int height;
 
-    public SpriteSheet(String identifier, byte[][]raster, int width, int height){
-        this.identifier=identifier;
+    public SpriteSheet(byte[][]raster, int width, int height){
         this.raster=raster;
         this.width=width;
         this.height=height;
-    }
-
-    public String getIdentifier(){
-        return identifier;
     }
 
     public byte[][]getRaster(){
